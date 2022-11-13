d0cf3c9523c41c48589dbee3589b2bbff7418ae1
Timothy <tzw202623@yahoo.com>
2022-11-12 17:43:04 -0800

    reorganize mock tests

--- tests/subtraction.test.js
+++ tests/subtraction.test.js
@@ -1,4 +1,4 @@
-const subtraction = require('../src/subtraction');
+const subtraction = require('./subtraction');
 test('subtracts 4 - 2 to equal 2', () => {
    expect(subtraction(4, 2)).toBe(2);
 });
