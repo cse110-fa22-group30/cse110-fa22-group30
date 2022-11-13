e80e21d66c56417975f50d18eccf3e9a116c61c2
Timothy <tzw202623@yahoo.com>
2022-11-12 17:36:09 -0800

    Redirect ci output to file

--- tests/subtraction.test.js
+++ tests/subtraction.test.js
@@ -1,4 +1,4 @@
-const subtraction = require('./subtraction');
+const subtraction = require('../src/subtraction');
 test('subtracts 4 - 2 to equal 2', () => {
    expect(subtraction(4, 2)).toBe(2);
 });
