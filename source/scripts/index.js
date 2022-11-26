7ae76a5260c1a2f3b68963e38dfb84e2a7fd3f43
Automated Publisher <actions@users.noreply.github.com>
2022-11-26 06:44:03 +0000

    Automated publish: Sat Nov 26 06:44:03 UTC 2022 1923611b3fa416622c74f89d133cacda62b7b854

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,15 +1,15 @@
-window.addEventListener("DOMContentLoaded", init);
+window.addEventListener('DOMContentLoaded', init)
 
-function init() {
-  console.log("in init");
-  console.log("test");
+function init () {
+  console.log('in init')
+  console.log('test')
 
-  const main = document.getElementById("scheduledContainer");
-  const exercise = document.createElement("exercise-card");
-  
-//   exercise.setAttribute('data', { foo: 'bar'})
-  exercise.data = { foo: "bar" };
+  const main = document.getElementById('scheduledContainer')
+  const exercise = document.createElement('exercise-card')
 
-  console.log("main", main);
-  main.appendChild(exercise);
+  //   exercise.setAttribute('data', { foo: 'bar'})
+  exercise.data = { foo: 'bar' }
+
+  console.log('main', main)
+  main.appendChild(exercise)
 }
