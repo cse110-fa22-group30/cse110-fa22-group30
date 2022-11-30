4334b661f55b2206541ce978811af411fdd5800f
SideCoin <yreganyang@gmail.com>
2022-11-29 22:39:54 -0800

    aded another function to test

--- source/scripts/jsdocTest.js
+++ source/scripts/jsdocTest.js
@@ -7,3 +7,14 @@ function d (a,b) {
 	console.log(a, b)
 }
 d(1, 1)
+
+/**
+ * 
+ * @param {number} a - does something dumb
+ * @param {number} b - idk moyai
+ */
+ function c (a,b) {
+	console.log(a, b)
+}
+c(1, 1)
+
