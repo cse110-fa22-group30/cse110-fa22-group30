c35e363465350e5abcf966ef96fbc1f8ca1512e5
Timothy <tzw202623@yahoo.com>
2022-12-06 19:50:14 -0800

    recover main

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -17,7 +17,7 @@ function initializeStorage () {
       'walking',
       'hiking',
       'skating',
-      'skiiing',
+      'skiing',
       'rowing',
       'elliptical',
       'stairmaster'
@@ -47,29 +47,8 @@ function initializeStorage () {
     }
   }
 
-  const data = [
-    // initial data sets
-    {
-      completed: 'true',
-      type: 'cycling', // must be lower case, match case, or implement .tolowercase()
-      date: '2023-01-01', // must be in this format to make coding the input of date easier
-      // duration: 2,
-      calories: 580,
-      stat1: 7500,
-      stat2: 85,
-      notes: 'Tour de France here I come!'
-    },
-    {
-      completed: 'false',
-      type: 'bench press', // must be lower case, match case, or implement .tolowercase()
-      date: '2021-01-01', // must be in this format to make coding the input of date easier
-      // duration: 2,
-      calories: 135,
-      stat1: 5,
-      stat2: 5,
-      notes: 'Remember not to skip leg day!!!'
-    }
-  ]
+  const data = []
+
   if (localStorage.getItem('cards') == null) {
     localStorage.setItem('cards', JSON.stringify(data))
   }
@@ -93,7 +72,6 @@ function addExerciseListener (scheduledContainer) {
         completed: 'false',
         type: '', // must be lower case, match case, or implement .tolowercase()
         date: yourDate.toISOString().split('T')[0], // must be in this format to make coding the input of date easier
-        // duration: '',
         calories: '',
         stat1: '',
         stat2: '',
@@ -110,9 +88,9 @@ function addExerciseListener (scheduledContainer) {
       )
     })
 }
-//TODO:Remove to make initialized page empty
+
 function init () {
-  // Populate page with 2 exercise cards
+  // Populate page
   const scheduledContainer = document.getElementById('scheduledContainer')
 
   const data = initializeStorage()
@@ -142,7 +120,7 @@ document.addToScheduled = function (exercise) {
   const scheduledContainer = document.getElementById('scheduledContainer')
   const scheduledList = scheduledContainer.getElementsByTagName('exercise-card')
 
-  // add elemenet to scheduled list based on date
+  // add element to scheduled list based on date
   if (scheduledList.length === 0) {
     scheduledContainer.appendChild(exercise)
   } else {
@@ -166,7 +144,7 @@ document.addToCompleted = function (exercise) {
   const completedContainer = document.getElementById('completedContainer')
   const completedList = completedContainer.getElementsByTagName('exercise-card')
 
-  // add elemenet to completed list based on date
+  // add element to completed list based on date
   if (completedList.length === 0) {
     completedContainer.appendChild(exercise)
   } else {
